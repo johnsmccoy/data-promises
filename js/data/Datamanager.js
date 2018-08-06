b79757e3118d3d5ac4fbf342bb7b2a29a68519d9
johnsmccoy <johnsmccoy@comcast.net>
2018-08-06 11:55:54 -0500

    fixed main.js

--- js/data/Datamanager.js
+++ js/data/Datamanager.js
@@ -1,25 +1,30 @@
-
-    // Purpose: Store and retrieve data from remote API
+/*
+    Purpose: Store and retrieve data from remote API
+*/
 
 const APIObject = {
 
 }
 
-
-
+/*
+    Purpose: Make GET request to API to retrieve data
+*/
 APIObject.getTypes = () => {
-   return fetch("http://localhost:8088/types")
-    .then(response => response.json())
+    return fetch("http://localhost:8088/types")
+        .then(response => response.json());
 }
 
-
+/*
+    Purpose: Retrieves all product objects from API
+*/
 APIObject.getProducts = () => {
-   return fetch("http://localhost:8088/inventory")
-    .then(response => response.json())
+    return fetch("http://localhost:8088/inventory")
+    .then(response => response.json());
 }
 
-
-
+/*
+    Purpose: POSTs (creates) a new product in the API
+*/
 APIObject.saveProduct = (product) => {
     return fetch("http://localhost:8088/inventory", {
         method: "POST",
@@ -30,3 +35,4 @@ APIObject.saveProduct = (product) => {
     });
 }
 
+module.exports = APIObject
---
+++ js/main.js
@@ -0,0 +1,1 @@
+const DataManager = require("./data/DataManager")
